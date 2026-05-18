9c855ba97ea6804db63e6314383f7c505153b9cc
Rohi <noelrohi59@gmail.com>
2026-05-19 02:21:13 +0800

    docs: add v0.6.4 release notes

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const DOWNLOAD_URL =
-  "https://github.com/noelrohi/dilag/releases/download/v0.6.3/Dilag-0.6.3-arm64.dmg"
+  "https://github.com/noelrohi/dilag/releases/download/v0.6.4/Dilag-0.6.4-arm64.dmg"
